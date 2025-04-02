950e089bb839292bddd11b982b70452f7a3151e7
carloozed <carlo@ozelot.ltd>
2025-04-02 12:38:48 +0200

    wtf

--- src/app/components/SectionComponents/StudioComponent/StudioComponent.tsx
+++ src/app/components/SectionComponents/StudioComponent/StudioComponent.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MainContainer from '../MainContainer';
 import StudioContent from './components/StudioContent';
 
+import { useContents } from '../../../../../context/ContentContext';
+
 export default function StudioComponent({
   isStudioActive,
   transitionEnd,
@@ -9,12 +11,17 @@ export default function StudioComponent({
   isStudioActive: boolean;
   transitionEnd: boolean;
 }) {
+  console.log(isStudioActive, transitionEnd);
+
+  const { studio } = useContents();
+
   return (
     <MainContainer>
       {isStudioActive && transitionEnd && (
         <StudioContent
           isStudioActive={isStudioActive}
           transitionEnd={transitionEnd}
+          studio={studio}
         />
       )}
     </MainContainer>
--- src/app/components/SectionComponents/StudioComponent/components/StudioContent.tsx
+++ src/app/components/SectionComponents/StudioComponent/components/StudioContent.tsx
@@ -5,46 +5,52 @@ import React from 'react';
 import { useContents } from '../../../../../../context/ContentContext';
 import { PrismicRichText } from '@prismicio/react';
 
+import { StudioDocument } from '../../../../../../prismicio-types';
+
 import styles from './StudioContent.module.css';
-import { PrismicNextLink } from '@prismicio/next';
 
 type Props = {
   isStudioActive: boolean;
   transitionEnd: boolean;
+  studio: StudioDocument;
 };
 
-export default function StudioContent({}: Props) {
-  const { studio } = useContents();
+export default function StudioContent({
+  isStudioActive,
+  transitionEnd,
+  studio,
+}: Props) {
+  console.log(studio);
 
   const data = studio.data;
   return (
     <div>
       <div className={styles.content}>
-        <div className={styles.leftContainer}>
-          <div className={styles.titleContainer}>
-            {data.description_group.map((item, index) => (
-              <div key={index}>
-                <PrismicRichText field={item.title} />
+        <div className={styles.upperContainer}>
+          <div className={styles.leftContainer}>
+            <div className={styles.titleContainer}>
+              {data.description_group.map((item, index) => (
+                <div key={index}>
+                  <PrismicRichText field={item.title} />
+                </div>
+              ))}
+            </div>
+            <div className={styles.infoContainer}>
+              <div className={styles.descriptionContainer}>
+                <PrismicRichText field={data.ozelot_description} />
               </div>
-            ))}
-          </div>
-          <div className={styles.infoContainer}>
-            <div className={styles.descriptionContainer}>
-              <PrismicRichText field={data.ozelot_description} />
-            </div>
-            <div className={styles.addressContainer}>
-              <PrismicRichText field={data.address_name} />
-              <PrismicRichText field={data.address_street} />
-              <PrismicRichText field={data.address_zip} />
-              <PrismicNextLink field={data.contact_link} />
+              <div className={styles.addressContainer}>
+                <PrismicRichText field={data.address_name} />
+                <PrismicRichText field={data.address_street} />
+                <PrismicRichText field={data.address_zip} />
+              </div>
             </div>
           </div>
-          <div className={styles.marqueeContainer}>
-            <div className={styles.marqueeUpper}></div>
-            <div className={styles.marqueeLower}></div>
-          </div>
+          <div className={styles.rightContainer}></div>
+        </div>{' '}
+        <div className={styles.lowerContainer}>
+          <div className={styles.marqueeContainer}></div>
         </div>
-        <div className={styles.rightContainer}></div>
       </div>
     </div>
   );
