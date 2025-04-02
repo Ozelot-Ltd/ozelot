3bc6b11117d2bdfd3165af9277d0d499cab7a905
carloozed <carlo@ozelot.ltd>
2025-04-02 11:39:13 +0200

    changes

--- src/app/components/SectionComponents/StudioComponent/components/StudioContent.tsx
+++ src/app/components/SectionComponents/StudioComponent/components/StudioContent.tsx
@@ -2,11 +2,50 @@
 
 import React from 'react';
 
+import { useContents } from '../../../../../../context/ContentContext';
+import { PrismicRichText } from '@prismicio/react';
+
+import styles from './StudioContent.module.css';
+import { PrismicNextLink } from '@prismicio/next';
+
 type Props = {
   isStudioActive: boolean;
   transitionEnd: boolean;
 };
 
 export default function StudioContent({}: Props) {
-  return <div>StudioContent</div>;
+  const { studio } = useContents();
+
+  const data = studio.data;
+  return (
+    <div>
+      <div className={styles.content}>
+        <div className={styles.leftContainer}>
+          <div className={styles.titleContainer}>
+            {data.description_group.map((item, index) => (
+              <div key={index}>
+                <PrismicRichText field={item.title} />
+              </div>
+            ))}
+          </div>
+          <div className={styles.infoContainer}>
+            <div className={styles.descriptionContainer}>
+              <PrismicRichText field={data.ozelot_description} />
+            </div>
+            <div className={styles.addressContainer}>
+              <PrismicRichText field={data.address_name} />
+              <PrismicRichText field={data.address_street} />
+              <PrismicRichText field={data.address_zip} />
+              <PrismicNextLink field={data.contact_link} />
+            </div>
+          </div>
+          <div className={styles.marqueeContainer}>
+            <div className={styles.marqueeUpper}></div>
+            <div className={styles.marqueeLower}></div>
+          </div>
+        </div>
+        <div className={styles.rightContainer}></div>
+      </div>
+    </div>
+  );
 }
