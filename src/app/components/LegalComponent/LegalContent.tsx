168f3fee7bc2565de6a6e63e4b57a2fd2089aaaf
carloozed <carlo@ozelot.ltd>
2025-06-18 15:01:12 +0200

    changes

--- src/app/components/LegalComponent/LegalContent.tsx
+++ src/app/components/LegalComponent/LegalContent.tsx
@@ -7,6 +7,8 @@ import { LegalDocument } from '../../../../prismicio-types';
 import { PrismicRichText } from '@prismicio/react';
 
 import { isLegalVisibleStore } from '@/app/stores/IsLegalVisible';
+import { PrismicNextLink } from '@prismicio/next';
+import Arrow from '../SvgComponents/Arrow/Arrow';
 
 type Props = {
   legal: LegalDocument;
@@ -31,6 +33,16 @@ export default function LegalContent({ legal }: Props) {
             <div className={styles.impressum}>
               <div className={styles.titlecontainer}>
                 <PrismicRichText field={legal.data.title} />
+              </div>
+              <div
+                style={{ display: 'flex', alignItems: 'center', gap: '0.5vw' }}
+              >
+                <PrismicNextLink field={legal.data.agb_download} />
+                <div>
+                  <div style={{ transform: ' translateY(2px) rotate(-45deg)' }}>
+                    <Arrow height="12" />
+                  </div>
+                </div>
               </div>
               {legal.data.categories.map((item, index) => (
                 <div key={index} className={styles.category}>
