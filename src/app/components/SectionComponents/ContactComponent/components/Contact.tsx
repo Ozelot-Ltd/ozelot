50529672a84e5dea0eeacf20505c80b9eb5822c6
carloozed <carlo@ozelot.ltd>
2025-09-26 17:00:29 +0200

    changes

--- src/app/components/SectionComponents/ContactComponent/components/Contact.tsx
+++ src/app/components/SectionComponents/ContactComponent/components/Contact.tsx
@@ -4,35 +4,24 @@ import React from 'react';
 import styles from './Contact.module.css';
 import { useContents } from '../../../../../../context/ContentContext';
 import { PrismicRichText } from '@prismicio/react';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-import { lottieSources } from '../../ServicesComponent/ServiceComponent/components/Service';
 import SocialBar from './SocialBar';
 import ContactForm from './ContactForm';
 import FadeIn from '@/app/components/FadeIn/FadeIn';
 import LegalComponent from '@/app/components/LegalComponent/LegalComponent';
 
 export default function Contact() {
-  const cube = lottieSources['3d'];
-
   const { contact } = useContents();
 
   return (
     <section className={styles.container}>
       <section className={styles.leftContainer}>
         <div className={styles.titleContainer}>
-          <FadeIn delay={0.8} yDown={500} duration={2}>
-            <div className={styles.title}>
-              <PrismicRichText field={contact.data.contact_title} />
-            </div>
-          </FadeIn>
-          <div className={styles.lottieContainer}>
-            <DotLottieReact autoplay loop src={cube} />
+          <div className={styles.textContainer}>
+            <PrismicRichText field={contact.data.contact_text} />
           </div>
         </div>
-        <div className={styles.textContainer}>
-          <PrismicRichText field={contact.data.contact_text} />
-        </div>
+
         <div className={styles.socialBarContainer}>
           <SocialBar />
         </div>
