380ccf2d2aeec99f511dad922905d3e3b913e351
carloozed <carlo@ozelot.ltd>
2025-09-29 19:16:03 +0200

    changes

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  allowedDevOrigins: [
+    'http://192.168.1.4:3000',
+    'http://192.168.1.4',
+    '192.168.1.4:3000',
+    '192.168.1.4',
+  ],
 };
 
 export default withBundleAnalyzer(nextConfig);
