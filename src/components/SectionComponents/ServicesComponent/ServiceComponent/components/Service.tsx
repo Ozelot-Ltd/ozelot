f40c586b6174ace72c91767c45d589d51632e3d2
carloozed <carlo@ozelot.ltd>
2026-03-18 11:19:28 +0100

    refactor: - title container sticky now

--- src/components/SectionComponents/ServicesComponent/ServiceComponent/components/Service.tsx
+++ src/components/SectionComponents/ServicesComponent/ServiceComponent/components/Service.tsx
@@ -1,29 +1,17 @@
-import { useRef } from 'react';
-
 import styles from './Service.module.css';
 import { ServicenewDocument } from '@/prismicio-types';
 import { PrismicRichText } from '@prismicio/react';
-
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import ServiceItems from './ServiceItems';
-
-gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 type Props = {
   service: ServicenewDocument;
 };
 
 const Service = ({ service }: Props) => {
-  const mainContainerRef = useRef<HTMLDivElement>(null);
-  const indexRef = useRef<HTMLHeadingElement>(null);
-  const leftContainerRef = useRef<HTMLDivElement>(null);
-
   return (
-    <div className={`${styles.container}`} ref={mainContainerRef}>
-      <div className={styles.leftcontainer} ref={leftContainerRef}>
-        <div className={styles.titlecontainer} ref={indexRef}>
+    <div className={styles.container}>
+      <div className={styles.leftcontainer}>
+        <div className={styles.titlecontainer}>
           <h2 className={styles.index}>0{service.data.index}</h2>
           <h2>–</h2>
           <div className={styles.titletext}>
